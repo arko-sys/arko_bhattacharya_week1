4c7e6b22f16086470f217a9239ad5bfd5062a612
Arko Bhattacharya <arko_bhattacharya@hotmail.com>
2024-09-03 03:00:38 -0400

    reformed vars

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,31 +1,32 @@
-import unittest
-import numpy as np
-import pandas as pd
-from io import StringIO
 import os
 import sys
+import numpy as np
+import pandas as pd
+import unittest
+from io import StringIO
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../src"))
 from main import add_2_arr, create_df, df_to_csv
 
 
-class TestFunctions(unittest.TestCase):
+class Test(unittest.TestCase):
 
     def test_add_2_arr(self):
         arr1 = np.array([1, 2, 3])
         arr2 = np.array([4, 5, 6])
-        expected_result = np.array([5, 7, 9])
-        np.testing.assert_array_equal(add_2_arr(arr1, arr2), expected_result)
+        res_arr = np.array([5, 7, 9])
+        np.testing.assert_array_equal(add_2_arr(arr1, arr2), res_arr)
 
     def test_create_df(self):
         arr1 = np.array([1, 2, 3])
         arr2 = np.array([4, 5, 6])
         arr3 = np.array([5, 7, 9])
         df = create_df(arr1, arr2, arr3)
-        expected_df = pd.DataFrame(
+        res_df = pd.DataFrame(
             {"Array 1": [1, 2, 3], "Array 2": [4, 5, 6], "Array 3": [5, 7, 9]}
         )
-        pd.testing.assert_frame_equal(df, expected_df)
+        pd.testing.assert_frame_equal(df, res_df)
 
     def test_df_to_csv(self):
         arr1 = np.array([1, 2, 3])
@@ -37,13 +38,13 @@ class TestFunctions(unittest.TestCase):
         buffer = StringIO()
         df_to_csv(df, buffer)
         buffer.seek(0)
-        saved_df = pd.read_csv(buffer)
+        res_df = pd.read_csv(buffer)
 
         expected_df = pd.DataFrame(
             {"Array 1": [1, 2, 3], "Array 2": [4, 5, 6], "Array 3": [5, 7, 9]}
         )
 
-        pd.testing.assert_frame_equal(saved_df, expected_df)
+        pd.testing.assert_frame_equal(res_df, expected_df)
 
 
 if __name__ == "__main__":
